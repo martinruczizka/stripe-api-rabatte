a1ff70fa54766dfb9dca0b439007ee32d104d030
Martin Ruczizka <79467093+martinruczizka@users.noreply.github.com>
2025-10-08 01:38:41 +0000

    Auto-Backup: 2025-10-08_0138

--- backups/commit-operativ.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /workspaces/stripe-product-mapper
-git remote set-url origin https://github.com/martinruczizka/stripe-api-rabatte.git
-git add --all
-git commit -m "Operatives Update: $(date +%Y-%m-%d_%H%M)"
-git push origin main
